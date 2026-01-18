ad1442e11e71ae08c286e90c1d3242c1772357d3
Nagu-developer-001 <168909081+Nagu-developer-001@users.noreply.github.com>
2026-01-18 07:41:32 +0530

    Time: 2 ms (89.77%), Space: 80.8 MB (23.01%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,25 +1,12 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> seen = new HashSet<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        int min =Integer.MAX_VALUE;
-        for(int num:nums1){
-            seen.add(num);
+        int i=0,j=0;
+        while(i<nums1.length &&j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }else if(nums1[i]<nums2[j]) i++;
+            else j++;
         }
-        for(int num:nums2){
-            if(seen.contains(num) && !result.contains(num))
-            {
-                result.add(num);
-            }
-        }
-         if (result.size() == 0) {
-            return -1;
-        }
-        for(int num:result){
-            if(num<min){
-                min = num;
-            }
-        }
-        return min;
+        return -1;
     }
 }
