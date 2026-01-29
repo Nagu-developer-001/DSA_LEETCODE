47c91a18c6aafdc2f565136c57c1eb8920ae7d85
Nagu-developer-001 <168909081+Nagu-developer-001@users.noreply.github.com>
2026-01-29 21:10:19 +0530

    Time: 1 ms (99.85%), Space: 46.8 MB (76.87%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,13 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int unique = print(nums);
-        return unique;
-
-    }
-    public int print(int[] nums){
         int unique = 0;
-        for(int i = 0;i < nums.length;i++){
-            unique ^=nums[i];
+        for(int i=0;i<nums.length;i++){
+            unique^=nums[i];
         }
         return unique;
     }
