d463ab36097d617f53fc252ea98982ac9d4effc0
Nagu-developer-001 <168909081+Nagu-developer-001@users.noreply.github.com>
2026-01-31 13:26:53 +0530

    Time: 1 ms (99.82%), Space: 55.6 MB (64.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,36 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n_by_2 = nums.length/2;
-        Map<Integer,Integer> seen = new HashMap<>();
+        int n = nums.length;
+        // Map<Integer,Integer> seen = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     seen.put(nums[i],seen.getOrDefault(nums[i],0)+1);
+        // }
+        // for(Map.Entry<Integer,Integer> mapEle : seen.entrySet()){
+        //     int key = mapEle.getKey();
+        //     int val = mapEle.getValue();
+        //     if(val>n_by_2){
+        //         return key;
+        //     }
+        // }
+        // return -1;
+        int freq = 0,ans = 0;
         for(int i=0;i<nums.length;i++){
-            seen.put(nums[i],seen.getOrDefault(nums[i],0)+1);
-        }
-        for(Map.Entry<Integer,Integer> mapEle : seen.entrySet()){
-            int key = mapEle.getKey();
-            int val = mapEle.getValue();
-            if(val>n_by_2){
-                return key;
+            if(freq == 0) {
+             ans = nums[i];
+             freq = 1;    
             }
+            else if(ans == nums[i]) freq++;
+            else freq--;
         }
-        return -1;
+    int count = 0;
+       for (int i = 0; i < n; i++) {
+           if (nums[i] == ans)
+               count++;
+       }
+       // Check if it occurs more than n/2 times
+       if (count > n / 2)
+           return ans;
+       else
+           return -1;
     }
 }
