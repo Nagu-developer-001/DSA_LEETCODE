f8150ca11f71344c1bad2e0580ad791cc3264c9c
Nagu-developer-001 <168909081+Nagu-developer-001@users.noreply.github.com>
2026-02-08 18:05:10 +0530

    Time: 10 ms (17.54%), Space: 47.1 MB (16.97%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,7 +1,17 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        while(s.indexOf(part)!=-1){
-            int pos = s.indexOf(part);
+        //int pos=-1;
+        while(true){
+            int pos = -1;
+            for(int i=0;i<=s.length()-part.length();i++){
+                if(s.substring(i,i+part.length()).equals(part)){
+                    pos = i;
+                    break;
+                }
+            }
+            //int pos = s.indexOf(part);
+            
+            if(pos==-1) break;
             s = s.substring(0,pos)+s.substring(pos+part.length());
         }
         return s;
