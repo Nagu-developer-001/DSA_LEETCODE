78c06a3ad0be1ab472f3ff99f98cbecfc88b190b
Nagabhushana N <168909081+Nagu-developer-001@users.noreply.github.com>
2026-05-25 12:49:34 +0530

    Time: 2 ms (95.23%), Space: 47.8 MB (25.6%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,26 +1,15 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
-        int sum_nums = n * (n+1)/2;
-        int sum_set = 0;
-        int missing = -1;
-        HashSet<Integer> set = new HashSet<>();
-        int[] arr = new int[2];
-        for(int i = 0; i < n ;i++)
-        {
-            if(set.contains(nums[i]))
-            {
-                missing = nums[i];
-            }else{
-                set.add(nums[i]);
-            }
+        int[] freq = new int[n+1];
+        for(int num : nums){
+            freq[num]++;
         }
-        for(int num : set){
-            sum_set+=num;
+        int missing = -1,duplicate = -1;
+        for(int i=1;i<=n;i++){
+            if(freq[i]==0)  missing=i;
+            if(freq[i]>1) duplicate =i;
         }
-        System.out.println(sum_set+" "+missing+" "+sum_nums+" "+n);
-        arr[0] = missing;
-        arr[1] = sum_nums - sum_set;
-        return arr;
+        return new int[]{duplicate,missing};
     }
 }
