9d44a9cbda1bc9fd1f81630da7afd86f4dd9015a
Nagabhushana N <168909081+Nagu-developer-001@users.noreply.github.com>
2026-05-30 21:47:37 +0530

    Time: 36 ms (29.63%), Space: 59.1 MB (73.5%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //int n = nums.length;
-        //int sum = 0;
+
+        List<List<Integer>>al = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> Result = new ArrayList<>();
-        Set<List<Integer>> uniqueSum = new HashSet<>(); 
-    for(int i=0;i<nums.length-2;i++){
-        int low = i+1;
-        int high = nums.length-1;
-        while(low<high){
-        //System.out.println("["+nums[i]+","+nums[low]+","+nums[high]+"] and n="+n+"k="+low+"j="+high+"i="+i);
-        //int sum = nums[i]+nums[low]+nums[high];
-        if(nums[i]+nums[low]+nums[high]==0){
-            List<Integer> triplet = Arrays.asList(nums[i],nums[low],nums[high]);
-                    //System.out.print(triplet);
-            Collections.sort(triplet);
-                    //System.out.print(triplet);
-            uniqueSum.add(triplet);
-            while(low<high && nums[low]==nums[low+1]){
-                low+=1;
-            }
-            while(low<high && nums[high]==nums[high-1]){
-                high-=1;
-            }
-            low+=1;
-            high-=1;
-                }
-        else if(nums[i]+nums[low]+nums[high]<0){
-            low+=1;
-        }
-        else if(nums[i]+nums[low]+nums[high]>0){
-            high-=1;
-        }
-        }
         
-    }
-        //i+=1;
-    Result.addAll(uniqueSum);
-    return Result;
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i]==nums[i-1])continue; 
+            int low = i+1;
+            int high = nums.length-1;
+            int target = -nums[i];
+            while(low<high){
+                if(nums[low]+nums[high]==target){
+                    al.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    low++;
+                    high--;
+                    while(low<high &&nums[low]==nums[low-1]){
+                        low++;
+                    }
+                    while(low<high && nums[high]==nums[high+1]){
+                        high--;
+                    }
+                }else if(nums[low]+nums[high]<target){
+                    low++;
+                }else{
+                    high--;
+                }
+            }         
+        }
+        return al;
     }
 }
